708254f100dcd693066225fda300a6b417620ecf
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-04-03 19:28:01 +0530

    Add c program to implement bubble sort

---
+++ 2nd_sem_lab_programs/bubbleSort.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    int a[100];
+    printf("Enter the elements: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted elements: \n");
+    for(int i=0;i<n;i++){
+        printf("%d",a[i]);
+        printf(" ");
+    }
+    return 0;
+}
